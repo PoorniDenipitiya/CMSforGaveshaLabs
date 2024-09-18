03dfcba2a8444c544635de9d08dfed031a4c0a50
PoorniDenipitiya <denipitiyadpu.21@uom.lk>
2024-09-19 02:51:54 +0530

    first commit

---
+++ backend/models/questions.js
@@ -0,0 +1,38 @@
+const { ObjectId } = require('mongodb');
+
+
+const mongoose = require ("mongoose");
+const Questions = new mongoose.Schema(
+    {
+        title:{
+            type:String, 
+            required : true,
+
+        }, 
+        description:{
+            type:String,
+            required : true,   
+        },
+        imageUrl:{ 
+            type:String,
+            required:false,
+        },
+        viewCount:{
+            type:Number, 
+            default:0
+        },
+        date:{
+            type:String,
+            required:true,
+        },
+        postedBy: {
+            type: ObjectId,
+            ref: "User",
+            required: false  
+        },
+    },
+    {
+        timestamps:true
+    }
+);
+module.exports=mongoose.model('questions',Questions);
